ea6925c4307f52c7f0361573e9153d5e17ffa4ff
IrinaT <irina.tiora@isa.utm.md>
2022-10-21 12:55:52 +0300

    Add endpoints for school

---
+++ api/school.go
@@ -0,0 +1,78 @@
+package api
+
+import (
+	"database/sql"
+	db "github.com/EliriaT/SchoolAppApi/db/sqlc"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type createSchoolRequest struct {
+	Name string `json:"name" binding:"required"`
+}
+
+func (server *Server) createSchool(ctx *gin.Context) {
+	var req createSchoolRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	school, err := server.store.CreateSchool(ctx, req.Name)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, school)
+}
+
+type getSchoolRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getSchoolbyId(ctx *gin.Context) {
+	var req getSchoolRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	school, err := server.store.GetSchoolbyId(ctx, req.ID)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, school)
+}
+
+type listSchoolRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listSchools(ctx *gin.Context) {
+	var req listSchoolRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListSchoolsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	school, err := server.store.ListSchools(ctx, arg)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, school)
+}
--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"github.com/EliriaT/SchoolAppApi/api"
+	db "github.com/EliriaT/SchoolAppApi/db/sqlc"
+	_ "github.com/lib/pq"
+	"log"
+)
 
+const (
+	dbDriver     = "postgres"
+	dbSource     = "postgresql://root:secret@localhost:5432/school?sslmode=disable"
+	serverAdress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+	err = server.Start(serverAdress)
+
+	if err != nil {
+		log.Fatal("server can not be started. ", err)
+	}
 }
